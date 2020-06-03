b060cabcf3a91071a3640027fb4401226150b938
Eric Sun <sunyonglincanada@gmail.com>
2020-06-02 22:58:35 -0400

    Init on Launch Item using api data.

--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
+import LaunchItem from './LaunchItem';
 
 const LAUNCHES_QUERY = gql`
   query LaunchesQuery {
@@ -15,17 +16,23 @@ const LAUNCHES_QUERY = gql`
 
 const Launches = () => {
   return (
-    <div>
+    <Fragment>
       <h1 className='display-4 my-3'>Launches</h1>
       <Query query={LAUNCHES_QUERY}>
         {({ loading, error, data }) => {
           if (loading) return <h4>Loading...</h4>;
           if (error) console.log(error);
 
-          return <h1>test</h1>;
+          return (
+            <Fragment>
+              {data.launches.map((launch) => (
+                <LaunchItem key={launch.flight_number} launch={launch} />
+              ))}
+            </Fragment>
+          );
         }}
       </Query>
-    </div>
+    </Fragment>
   );
 };
 
