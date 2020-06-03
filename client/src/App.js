4aec074f380eeb1e37c955fc60bd4f89092cf86d
Eric Sun <sunyonglincanada@gmail.com>
2020-06-02 22:37:35 -0400

    Init setup apollo.

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import logo from './logo.png';
 import './App.css';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql'
+});
+
 function App() {
   return (
-    <div className='App'>
-      <img
-        src={logo}
-        alt='SpaceX'
-        style={{ width: 300, display: 'block', margin: 'auto' }}
-      />
-    </div>
+    <ApolloProvider client={client}>
+      <div className='container'>
+        <img
+          src={logo}
+          alt='SpaceX'
+          style={{ width: 300, display: 'block', margin: 'auto' }}
+        />
+      </div>
+    </ApolloProvider>
   );
 }
 
