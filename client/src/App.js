ac89071f554547fb617d4db7326d630eaadecab2
Eric Sun <sunyonglincanada@gmail.com>
2020-06-15 19:45:19 -0400

    Install react router and add to client side.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Launches from './components/Launches';
 import logo from './logo.png';
 import './App.css';
@@ -12,14 +13,16 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className='container'>
-        <img
-          src={logo}
-          alt='SpaceX'
-          style={{ width: 300, display: 'block', margin: 'auto' }}
-        />
-        <Launches />
-      </div>
+      <Router>
+        <div className='container'>
+          <img
+            src={logo}
+            alt='SpaceX'
+            style={{ width: 300, display: 'block', margin: 'auto' }}
+          />
+          <Route path='/' component={Launches} />
+        </div>
+      </Router>
     </ApolloProvider>
   );
 }
